8fbd93ba37ed8163102135f27d68dd3da1623526
Rodrigo Coelho <95186002+rodrigopcoelho@users.noreply.github.com>
2024-03-11 09:44:56 +0000

    index fixed

--- src/index.js
+++ src/index.js
@@ -3,17 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
